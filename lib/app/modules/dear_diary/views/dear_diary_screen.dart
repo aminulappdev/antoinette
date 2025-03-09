6f1c7e0cdaa04f074f0a45b90af93471c045daca
Md Aminul Islam <aminulappdev@gmail.com>
2025-03-09 13:12:27 +0600

    9/03/2025

--- lib/app/modules/dear_diary/views/dear_diary_screen.dart
+++ lib/app/modules/dear_diary/views/dear_diary_screen.dart
@@ -15,13 +15,23 @@ class DearDiaryScreen extends StatefulWidget {
 }
 
 class _DearDiaryScreenState extends State<DearDiaryScreen> {
+  bool _isDropdownOpen = false;
+  String _selectedMonth = 'January';
+
+  void _toggleDropdown() {
+    setState(() {
+      _isDropdownOpen = !_isDropdownOpen; 
+    });
+  }
+
   List<Color> colors = [
-    Color(0XFF5AE205).withOpacity(0.30),
-    Color(0XFFFF3434).withOpacity(0.30),
-    Color(0XFFE9BD0F).withOpacity(0.30),
     Color(0XFF344BFD).withOpacity(0.30),
     Color(0XFFC000E7).withOpacity(0.30),
     Color(0XFFFF9318).withOpacity(0.30),
+    Color(0XFF5AE205).withOpacity(0.30),
+    Color(0XFFFF3434).withOpacity(0.30),
+    Color(0XFFE9BD0F).withOpacity(0.30),
+    
   ];
   Map<String, double> dataMap = {
     "Flutter": 1,
@@ -56,6 +66,7 @@ class _DearDiaryScreenState extends State<DearDiaryScreen> {
                   padding:
                       const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Stack(
                         children: [
@@ -88,10 +99,10 @@ class _DearDiaryScreenState extends State<DearDiaryScreen> {
                             child: CircleAvatar(
                               radius: 15,
                               backgroundColor: Colors.transparent,
-                              backgroundImage: AssetImage(AssetsPath.happy),
+                              backgroundImage: AssetImage(AssetsPath.sad),
                             ),
                           ),
-                           Positioned(
+                          Positioned(
                             left: 4,
                             bottom: 32,
                             child: CircleAvatar(
@@ -100,27 +111,25 @@ class _DearDiaryScreenState extends State<DearDiaryScreen> {
                               backgroundImage: AssetImage(AssetsPath.muscle),
                             ),
                           ),
-                           Positioned(
+                          Positioned(
                             left: 4,
                             bottom: 100,
                             child: CircleAvatar(
                               radius: 15,
                               backgroundColor: Colors.transparent,
-                              backgroundImage: AssetImage(AssetsPath.sad),
+                              backgroundImage: AssetImage(AssetsPath.happy),
                             ),
                           ),
-                           Positioned(
+                          Positioned(
                             top: 4,
                             left: 54,
-
                             child: CircleAvatar(
-                              
                               radius: 15,
                               backgroundColor: Colors.transparent,
-                              backgroundImage: AssetImage(AssetsPath.tired),
+                              backgroundImage: AssetImage(AssetsPath.angry),
                             ),
                           ),
-                           Positioned(
+                          Positioned(
                             left: 104,
                             top: 30,
                             child: CircleAvatar(
@@ -129,25 +138,198 @@ class _DearDiaryScreenState extends State<DearDiaryScreen> {
                               backgroundImage: AssetImage(AssetsPath.angle),
                             ),
                           ),
-                           Positioned(
+                          Positioned(
                             left: 54,
                             bottom: 4,
                             child: CircleAvatar(
                               radius: 15,
                               backgroundColor: Colors.transparent,
-                              backgroundImage: AssetImage(AssetsPath.angry),
+                              backgroundImage: AssetImage(AssetsPath.tired),
                             ),
                           ),
-                           
-
                         ],
                       ),
+                      SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              height: 27,
+                              width: 90,
+                              decoration: BoxDecoration(
+                                color: Colors.purple,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(6.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      _selectedMonth,
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      onTap: _toggleDropdown,
+                                      child: CircleAvatar(
+                                        radius: 8,
+                                        backgroundColor: Colors.white,
+                                        child: Center(
+                                          child: Icon(
+                                            _isDropdownOpen
+                                                ? Icons.arrow_drop_up
+                                                : Icons.arrow_drop_down,
+                                            color: Colors.purple,
+                                            size: 16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            if (_isDropdownOpen) ...[
+                              // Dropdown list of months
+                              Column(
+                                children: [
+                                  for (var month in [
+                                    'January',
+                                    'February',
+                                    'March',
+                                    'April',
+                                    'May',
+                                    'June',
+                                    'July',
+                                    'August',
+                                    'September',
+                                    'October',
+                                    'November',
+                                    'December',
+                                  ])
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedMonth = month;
+                                          _isDropdownOpen = false;
+                                        });
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(2.0),
+                                        child: Container(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 2, horizontal: 8),
+                                          decoration: BoxDecoration(
+                                            color: Colors.purple.shade100,
+                                            borderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                          child: Text(
+                                            month,
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              )
+                            ],
+                            heightBox12,
+                            Row(
+                              children: [
+                                Column(
+                                  children: [
+                                    StatusCard(
+                                      percent: '30%',
+                                      emojiPath: AssetsPath.angry,
+                                    ),
+                                    heightBox10,
+                                    StatusCard(
+                                      percent: '30%',
+                                      emojiPath: AssetsPath.angry,
+                                    ),
+                                    heightBox10,
+                                    StatusCard(
+                                      percent: '30%',
+                                      emojiPath: AssetsPath.angry,
+                                    ),
+                                  ],
+                                ),
+                                widthBox14,
+                                Column(
+                                  children: [
+                                    StatusCard(
+                                      percent: '30%',
+                                      emojiPath: AssetsPath.angry,
+                                    ),
+                                    heightBox10,
+                                    StatusCard(
+                                      percent: '30%',
+                                      emojiPath: AssetsPath.angry,
+                                    ),
+                                    heightBox10,
+                                    StatusCard(
+                                      percent: '30%',
+                                      emojiPath: AssetsPath.angry,
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
-              )
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class StatusCard extends StatelessWidget {
+  final String emojiPath;
+  final String percent;
+  const StatusCard({
+    super.key,
+    required this.emojiPath,
+    required this.percent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 32,
+      width: 64,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 13,
+              backgroundColor: Colors.transparent,
+              backgroundImage: AssetImage(emojiPath),
+            ),
+            widthBox4,
+            Text(percent)
+          ],
         ),
       ),
     );
