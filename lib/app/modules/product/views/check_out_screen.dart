bb1dcd34a20dfe1db40090ce4de9726ffa23bfd6
Md Aminul Islam <aminulappdev@gmail.com>
2025-05-25 10:13:27 +0600

    update

--- lib/app/modules/product/views/check_out_screen.dart
+++ lib/app/modules/product/views/check_out_screen.dart
@@ -16,7 +16,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CheckOutScreen extends StatefulWidget {
-  final ProductModel productModel;
+  final ProductModel productModel; 
   static const String routeName = '/checkout-screen';
   const CheckOutScreen({super.key, required this.productModel});
 
